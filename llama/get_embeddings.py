c38d1ba773be054eba454e20e39b0bed433f8cfa
Akiho Kawada <kawadatakeshiho@gmail.com>
2023-07-26 12:43:02 +0900

    fix: get_embeddingsの変更

--- llama/get_embeddings.py
+++ llama/get_embeddings.py
@@ -1,114 +1,311 @@
 import torch  # type: ignore
-from model import ModelArgs  # type: ignore
-from tokenizer import Tokenizer  # type: ignore
-from fairscale.nn.model_parallel.initialize import (
+from llama.model import ModelArgs  # type: ignore
+from llama.tokenizer import Tokenizer  # type: ignore
+from fairscale.nn.model_parallel.initialize import (  # type: ignore
     get_model_parallel_rank,
     initialize_model_parallel,
     model_parallel_is_initialized,
 )
-from fairscale.nn.model_parallel.layers import (
+from fairscale.nn.model_parallel.layers import (  # type: ignore
     ParallelEmbedding,
 )
 import os
 import sys
 import json
+import torch.nn as nn  # type: ignore
+from llama.model import TransformerBlock, RMSNorm, ColumnParallelLinear, precompute_freqs_cis
+from typing import List, Optional, Tuple
+from pathlib import Path
+import time
+import torch.nn.functional as F  # type: ignore
+
+
+class Transformer(nn.Module):
+    def __init__(self, params: ModelArgs):
+        super().__init__()
+        self.params = params
+        self.vocab_size = params.vocab_size
+        self.n_layers = params.n_layers
+
+        self.tok_embeddings = ParallelEmbedding(
+            params.vocab_size, params.dim, init_method=lambda x: x
+        )
+
+        self.layers = torch.nn.ModuleList()
+        for layer_id in range(params.n_layers):
+            self.layers.append(TransformerBlock(layer_id, params))
+
+        self.norm = RMSNorm(params.dim, eps=params.norm_eps)
+        self.output = ColumnParallelLinear(
+            params.dim, params.vocab_size, bias=False, init_method=lambda x: x
+        )
+
+        self.freqs_cis = precompute_freqs_cis(
+            self.params.dim // self.params.n_heads, self.params.max_seq_len * 2
+        )
+
+    @torch.inference_mode()
+    def forward(self, tokens: torch.Tensor, start_pos: int):
+        _bsz, seqlen = tokens.shape
+        h = self.tok_embeddings(tokens)
+        embeddings=h
+        #self.freqs_cis = self.freqs_cis.to(h.device)
+        #freqs_cis = self.freqs_cis[start_pos : start_pos + seqlen]
+
+        #mask = None
+        #if seqlen > 1:
+            #mask = torch.full(
+                #(1, 1, seqlen, seqlen), float("-inf"), device=tokens.device
+            #)
+            #mask = torch.triu(mask, diagonal=start_pos + 1).type_as(h)
+
+        #for layer in self.layers:
+            # h = layer(h, start_pos, freqs_cis, mask)
+        #h = self.norm(h)
+        #output = self.output(h).float()
+
+        #print("embeddings.shape:", embeddings.shape)
+        #return output, embeddings
+        return embeddings
+
+
+class Llama:
+    @staticmethod
+    def build(
+        ckpt_dir: str,
+        tokenizer_path: str,
+        max_seq_len: int,
+        max_batch_size: int,
+        model_parallel_size: Optional[int] = None,
+    ) -> "Llama":
+        if not torch.distributed.is_initialized():
+            torch.distributed.init_process_group("nccl")
+        if not model_parallel_is_initialized():
+            if model_parallel_size is None:
+                model_parallel_size = int(os.environ.get("WORLD_SIZE", 1))
+            initialize_model_parallel(model_parallel_size)
+
+        local_rank = int(os.environ.get("LOCAL_RANK", 0))
+        torch.cuda.set_device(local_rank)
+
+        # seed must be the same in all processes
+        torch.manual_seed(1)
+
+        if local_rank > 0:
+            sys.stdout = open(os.devnull, "w")
+
+        start_time = time.time()
+        checkpoints = sorted(Path(ckpt_dir).glob("*.pth"))
+        assert len(checkpoints) > 0, f"no checkpoint files found in {ckpt_dir}"
+        assert model_parallel_size == len(
+            checkpoints
+        ), f"Loading a checkpoint for MP={len(checkpoints)} but world size is {model_parallel_size}"
+        ckpt_path = checkpoints[get_model_parallel_rank()]
+        checkpoint = torch.load(ckpt_path, map_location="cpu")
+        with open(Path(ckpt_dir) / "params.json", "r") as f:
+            params = json.loads(f.read())
+
+        model_args: ModelArgs = ModelArgs(
+            max_seq_len=max_seq_len,
+            max_batch_size=max_batch_size,
+            **params,
+        )
+        tokenizer = Tokenizer(model_path=tokenizer_path)
+        model_args.vocab_size = tokenizer.n_words
+        torch.set_default_tensor_type(torch.cuda.HalfTensor)
+        model = Transformer(model_args)
+        model.load_state_dict(checkpoint, strict=False)
+        print(f"Loaded in {time.time() - start_time:.2f} seconds")
+
+        return Llama(model, tokenizer)
+
+    def __init__(self, model: Transformer, tokenizer: Tokenizer):
+        self.model = model
+        self.tokenizer = tokenizer
+
+    @torch.inference_mode()
+    def get_embeddings(
+        self,
+        prompts: List[str],
+        max_gen_len: int,
+        logprobs: bool = False,
+    ) -> Tuple[List[List[int]], Optional[List[List[float]]]]:
+
+        prompt_tokens = [self.tokenizer.encode(x, bos=True, eos=False) for x in prompts]
+        params = self.model.params
+        bsz = len(prompt_tokens)
+        assert bsz <= params.max_batch_size, (bsz, params.max_batch_size)
+
+        min_prompt_len = min(len(t) for t in prompt_tokens)
+        max_prompt_len = max(len(t) for t in prompt_tokens)
+        assert max_prompt_len <= params.max_seq_len
+        total_len = min(params.max_seq_len, max_gen_len + max_prompt_len)
+
+        pad_id = self.tokenizer.pad_id
+        tokens = torch.full((bsz, total_len), pad_id, dtype=torch.long, device="cuda")
+        for k, t in enumerate(prompt_tokens):
+            tokens[k, : len(t)] = torch.tensor(t, dtype=torch.long, device="cuda")
+        if logprobs:
+            token_logprobs = torch.zeros_like(tokens, dtype=torch.float)
+
+        prev_pos = 0
+        eos_reached = torch.tensor([False] * bsz, device="cuda")
+        input_text_mask = tokens != pad_id
+        embeddings=self.model.forward(tokens, prev_pos)
+        #return embeddings
+        for cur_pos in range(min_prompt_len, total_len):
+            logits = self.model.forward(tokens[:, prev_pos:cur_pos], prev_pos)
+            print(logits.shape)
+            next_token = torch.where(input_text_mask[:, cur_pos], tokens[:, cur_pos], next_token)
+            eos_reached |= (~input_text_mask[:, cur_pos]) & (next_token == self.tokenizer.eos_id)
+            prev_pos = cur_pos
+            if all(eos_reached):
+                break
+
+
+
+
+    @torch.inference_mode()
+    def generate(
+        self,
+        prompt_tokens: List[List[int]],
+        max_gen_len: int,
+        temperature: float = 0.6,
+        top_p: float = 0.9,
+        logprobs: bool = False,
+        echo: bool = False,
+    ) -> Tuple[List[List[int]], Optional[List[List[float]]]]:
+        params = self.model.params
+        bsz = len(prompt_tokens)
+        assert bsz <= params.max_batch_size, (bsz, params.max_batch_size)
+
+        min_prompt_len = min(len(t) for t in prompt_tokens)
+        max_prompt_len = max(len(t) for t in prompt_tokens)
+        assert max_prompt_len <= params.max_seq_len
+        total_len = min(params.max_seq_len, max_gen_len + max_prompt_len)
+
+        pad_id = self.tokenizer.pad_id
+        tokens = torch.full((bsz, total_len), pad_id, dtype=torch.long, device="cuda")
+        for k, t in enumerate(prompt_tokens):
+            tokens[k, : len(t)] = torch.tensor(t, dtype=torch.long, device="cuda")
+        if logprobs:
+            token_logprobs = torch.zeros_like(tokens, dtype=torch.float)
+
+        prev_pos = 0
+        eos_reached = torch.tensor([False] * bsz, device="cuda")
+        input_text_mask = tokens != pad_id
+        for cur_pos in range(min_prompt_len, total_len):
+            logits = self.model.forward(tokens[:, prev_pos:cur_pos], prev_pos)
+            if logprobs:
+                token_logprobs[:, prev_pos + 1 : cur_pos + 1] = -F.cross_entropy(
+                    input=logits.transpose(1, 2),
+                    target=tokens[:, prev_pos + 1 : cur_pos + 1],
+                    reduction="none",
+                    ignore_index=pad_id,
+                )
+            if temperature > 0:
+                probs = torch.softmax(logits[:, -1] / temperature, dim=-1)
+                next_token = sample_top_p(probs, top_p)
+            else:
+                next_token = torch.argmax(logits[:, -1], dim=-1)
+
+            next_token = next_token.reshape(-1)
+            # only replace token if prompt has already been generated
+            next_token = torch.where(
+                input_text_mask[:, cur_pos], tokens[:, cur_pos], next_token
+            )
+            tokens[:, cur_pos] = next_token
+            eos_reached |= (~input_text_mask[:, cur_pos]) & (
+                next_token == self.tokenizer.eos_id
+            )
+            prev_pos = cur_pos
+            if all(eos_reached):
+                break
+
+        if logprobs:
+            token_logprobs = token_logprobs.tolist()
+        out_tokens, out_logprobs = [], []
+        for i, toks in enumerate(tokens.tolist()):
+            # cut to max gen len
+            start = 0 if echo else len(prompt_tokens[i])
+            toks = toks[start : len(prompt_tokens[i]) + max_gen_len]
+            probs = None
+            if logprobs:
+                probs = token_logprobs[i][start : len(prompt_tokens[i]) + max_gen_len]
+            # cut to eos tok if any
+            if self.tokenizer.eos_id in toks:
+                eos_idx = toks.index(self.tokenizer.eos_id)
+                toks = toks[:eos_idx]
+                probs = probs[:eos_idx] if logprobs else None
+            out_tokens.append(toks)
+            out_logprobs.append(probs)
+        return (out_tokens, out_logprobs if logprobs else None)
+
+    def text_completion(
+        self,
+        prompts: List[str],
+        temperature: float = 0.6,
+        top_p: float = 0.9,
+        max_gen_len: Optional[int] = None,
+        logprobs: bool = False,
+        echo: bool = False,
+    ) -> List:
+        if max_gen_len is None:
+            max_gen_len = self.model.params.max_seq_len - 1
+        prompt_tokens = [self.tokenizer.encode(x, bos=True, eos=False) for x in prompts]
+        generation_tokens, generation_logprobs = self.generate(
+            prompt_tokens=prompt_tokens,
+            max_gen_len=max_gen_len,
+            temperature=temperature,
+            top_p=top_p,
+            logprobs=logprobs,
+            echo=echo,
+        )
+        if logprobs:
+            return [
+                {
+                    "generation": self.tokenizer.decode(t),
+                    "tokens": [self.tokenizer.decode(x) for x in t],
+                    "logprobs": logprobs_i,
+                }
+                for t, logprobs_i in zip(generation_tokens, generation_logprobs)
+            ]
+        return [{"generation": self.tokenizer.decode(t)} for t in generation_tokens]
+
+def sample_top_p(probs, p):
+    probs_sort, probs_idx = torch.sort(probs, dim=-1, descending=True)
+    probs_sum = torch.cumsum(probs_sort, dim=-1)
+    mask = probs_sum - probs_sort > p
+    probs_sort[mask] = 0.0
+    probs_sort.div_(probs_sort.sum(dim=-1, keepdim=True))
+    next_token = torch.multinomial(probs_sort, num_samples=1)
+    next_token = torch.gather(probs_idx, -1, next_token)
+    return next_token
+
 
 max_seq_len=128
 max_batch_size=4
+max_gen_len: int = 64
+
+prompts=["Hello, how are you?"]
+generator = Llama.build(
+        ckpt_dir="/app/weight/llama-2-7b/",
+        tokenizer_path="/app/weight/tokenizer.model",
+        max_seq_len=max_seq_len,
+        max_batch_size=max_batch_size,
+)
+print("build!")
+
+results = generator.get_embeddings(
+        prompts,
+        max_gen_len=max_gen_len)
 
 
-
-# モデルの初期化
-# model = Transformer(params)
-# model.load_state_dict(torch.load("path_to_pretrained_model.pth"))  # TODO
-
-
-if not torch.distributed.is_initialized():
-    torch.distributed.init_process_group("nccl")
-if not model_parallel_is_initialized():
-    # if model_parallel_size is None:
-    model_parallel_size = int(os.environ.get("WORLD_SIZE", 1))
-    initialize_model_parallel(model_parallel_size)
-
-local_rank = int(os.environ.get("LOCAL_RANK", 0))
-torch.cuda.set_device(local_rank)
-
-torch.manual_seed(1)
-
-if local_rank > 0:
-    sys.stdout = open(os.devnull, "w")
-
-print("local_rank:", local_rank)
-
-# モデルの引数を設定
-with open("/app/weight/llama-2-7b/params.json", "r") as f:
-    params = json.loads(f.read())
-params=ModelArgs(max_seq_len=max_seq_len, max_batch_size=max_batch_size, **params,)
-
-
-# SentencePieceモデルをロード
-tokenizer = Tokenizer("/app/weight/tokenizer.model")  # TODO
-params.vocab_size = tokenizer.n_words
-torch.set_default_tensor_type(torch.cuda.HalfTensor)
-
-
-vocab_size = params.vocab_size
-tok_embeddings = ParallelEmbedding(params.vocab_size, params.dim, init_method=lambda x: x)
-
-# 特定の文字列をトークン化して、トークンIDに変換する
-text = "Hello, how are you?"  # TODO?
-#tokens = tokenizer.encode(text, bos=True, eos=True)
-prompt_tokens = [tokenizer.encode(x, bos=True, eos=False) for x in text]
-print("prompt_tokens:", prompt_tokens)
-# prompt_tokens: [[1, 379], [1, 321], [1, 301], [1, 301], [1, 288], [1, 1919], [1, 259], [1, 298], [1, 288], [1, 281], [1, 259], [1, 263], [1, 364], [1, 321], [1, 259], [1, 343], [1, 288], [1, 318], [1, 1577]]
-
-bsz = len(prompt_tokens)
-min_prompt_len = min(len(t) for t in prompt_tokens)
-max_prompt_len = max(len(t) for t in prompt_tokens)
-assert max_prompt_len <= params.max_seq_len
-
-max_gen_len: int = 64 # TODO?
-total_len = min(params.max_seq_len, max_gen_len + max_prompt_len)
-
-pad_id = tokenizer.pad_id
-tokens = torch.full((bsz, total_len), pad_id, dtype=torch.long, device="cuda")
-for k, t in enumerate(prompt_tokens):
-    tokens[k, : len(t)] = torch.tensor(t, dtype=torch.long, device="cuda")
-
-print("tokens type:", type(tokens))  # <class 'torch.Tensor'>
-print("tokens.shape: ", tokens.shape) # torch.Size([19, 66])
-print("tokens: ",tokens)
-# tokens:  tensor([[   1,  379,   -1,  ...,   -1,   -1,   -1],
-        #[   1,  321,   -1,  ...,   -1,   -1,   -1],
-        #[   1,  301,   -1,  ...,   -1,   -1,   -1],
-        #...,
-        #[   1,  288,   -1,  ...,   -1,   -1,   -1],
-        #[   1,  318,   -1,  ...,   -1,   -1,   -1],
-        #[   1, 1577,   -1,  ...,   -1,   -1,   -1]])
-prev_pos = 0
-eos_reached = torch.tensor([False] * bsz, device="cuda")
-input_text_mask = tokens != pad_id
-
-# print("min_prompt_len, total_len:", min_prompt_len, total_len) #2,66
-# for cur_pos in range(min_prompt_len, total_len):
-# h = tok_embeddings(tokens[:, prev_pos:cur_pos])
-h = tok_embeddings(tokens)
-print("type(h):", type(h)) # <class 'torch.Tensor'>
-print("h:", h)
-
-
-
-
-
-
-# トークンIDをPyTorchテンソルに変換
-# tokens_tensor = torch.tensor(tokens).unsqueeze(0)
-
-# with torch.no_grad():
-    # model_output = model(tokens_tensor, start_pos=0)
-    # embedding = model_output[0]  # 最初のサンプル（1つのテキスト）のembeddingを取得
-
-
-print("元のテキスト:", text)
-#print("embeddingのサイズ:", embedding.size())
-#print("embedding:", embedding)
+#results = generator.text_completion(
+        #prompts,
+        #max_gen_len=max_gen_len,
+        #temperature=0.6,
+        #top_p=0.9,
+#)
+#print(results)
+print("done!")
